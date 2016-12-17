9974da7bcbcaa051eee18004021250b3a39411a3
Frank A. Krueger <fak@praeclarum.org>
2016-12-16 22:51:58 -0800

    Add toggling GPIO 17

--- pi/app.py
+++ pi/app.py
@@ -1,11 +1,53 @@
 #!/usr/bin/python
 
 import sys
+import threading
 import spidev
 import time
 import urllib2
 import urllib
+import RPi.GPIO as GPIO
 
+# From: http://stackoverflow.com/a/16368571/338
+def setInterval(interval):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            stopped = threading.Event()
+            def loop(): # executed in another thread
+                while not stopped.wait(interval): # until stopped
+                    function(*args, **kwargs)
+            t = threading.Thread(target=loop)
+            t.daemon = True # stop if the program exits
+            t.start()
+            return stopped
+        return wrapper
+    return decorator
+
+#
+# Setup Valve GPIO
+#
+GPIO.setmode(GPIO.BCM)
+VALVE_GPIO = 17
+VALVE_STATE = False
+GPIO.setup(VALVE_GPIO, GPIO.OUT)
+GPIO.output(VALVE_GPIO, False)
+
+def setValve(state):
+  global VALVE_STATE
+  VALVE_STATE = state
+  GPIO.output(VALVE_GPIO, VALVE_STATE)
+
+@setInterval(2.0)
+def toggleValve():
+  global VALVE_STATE
+  setValve(not VALVE_STATE)
+  #print "T=", VALVE_STATE
+
+stopValve = toggleValve()
+
+#
+# Setup SPI
+#
 spi = spidev.SpiDev()
 spi.open(0, 0)
 
@@ -24,17 +66,20 @@ def readadc12(adcnum):
   a = ((r[1] & 15) << 8) + r[2]
   return a
 
+lastValveOnTime = time.localtime()
 while True:
   # HUMIDITY
   try:
-    t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    now = time.localtime()
+    t = time.strftime("%Y-%m-%d %H:%M:%S", now)
     print "TIME", t
     for x in range(0, 4):
       value = readadc12(x)
       logvalue("H" + str(x), 4095 - value, t)
       time.sleep(0.5)
+    logvalue("V", 1 if VALVE_STATE else 0, t)
   except:
     print "Unexpected error:", sys.exc_info()[0]
     #raise
-  time.sleep(5*60)
+  time.sleep(60)
 
